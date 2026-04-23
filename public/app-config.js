c01700ab65e2e99395021991252f924720de770f
Sheref Aboelhassan <sheref.aboelhassan1@gmail.com>
2026-04-23 22:16:25 +0200

    Set explicit auth redirect URL

--- public/app-config.js
+++ public/app-config.js
@@ -1,4 +1,5 @@
 window.MATRIX_ARENA_CONFIG = {
   supabaseUrl: "https://kdbljhwkwyyaasoayyjy.supabase.co",
-  supabaseAnonKey: "sb_publishable_cgt3VvVwnRp5KJztCNQvjw__Cp_3eOv"
+  supabaseAnonKey: "sb_publishable_cgt3VvVwnRp5KJztCNQvjw__Cp_3eOv",
+  siteUrl: "https://linearalgebragamified.netlify.app"
 };
